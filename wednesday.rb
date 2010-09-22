77050664b53848164c2d827c84a97b03852ae279
ben@benatkin.com <ben@benatkin.com>
2010-09-22 17:04:45 -0600

    added spec and parser/renderer class

--- wednesday.rb
+++ wednesday.rb
@@ -1,6 +1,18 @@
 require 'sinatra'
 require 'haml'
 
+class WednesdayTweet
+  attr_accessor :text
+
+  def initialize(text)
+    self.text = text
+  end
+
+  def entities
+    []
+  end
+end
+
 set :root, File.expand_path(File.dirname(__FILE__))
 
 set :haml, {:format => :html5 }
---
+++ wednesday_spec.rb
@@ -0,0 +1,10 @@
+require './wednesday'
+
+QUIET_TWEET = "#WatchWednesday *jashkenas coffee-script docco *cloudhead less *github gollum *creationix postgres-js http://watchwednesday.com/benatkin/1"
+
+describe WednesdayTweet, "#entities" do
+  it "returns the correct data" do
+    tweet = WednesdayTweet.new QUIET_TWEET
+    tweet.entities.should == :generated_data
+  end
+end
