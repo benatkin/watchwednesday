5405b35ad28ddab2736e3cf2a9475ff526a14204
ben@benatkin.com <ben@benatkin.com>
2010-09-22 19:15:10 -0600

    implemented finding entities

--- wednesday.rb
+++ wednesday.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'haml'
 
+ENTITY_RE = /(?:\s|\A)([*@]?[\w-]+)(?:\s|\Z)/
+
 class WednesdayTweet
   attr_accessor :text
 
@@ -9,7 +11,38 @@ class WednesdayTweet
   end
 
   def entities
-    []
+    user = nil
+    a = []
+    offset = 0
+    loop do
+      break if not text[offset..-1]
+      match = text[offset..-1].match(ENTITY_RE)
+      break if not match
+
+      capture = match.captures[0]
+      entity = {
+        :text  => capture,
+        :begin => offset + match.begin(0),
+        :end   => offset + match.end(0),
+        :url   => if capture[0..0] == '*' then
+                    user = capture[1..-1]
+                    'http://github.com/' + user
+                  elsif capture[0..0] == '@' then
+                    user = capture[1..-1]
+                    'http://twitter.com/' + user
+                  else
+                    user && 'http://github.com/' + user + '/' + capture
+                  end
+      }
+      a << entity
+
+      offset += (match.pre_match + match.to_s).length
+    end
+    a
+  end
+
+  def render
+    entities
   end
 end
 
--- wednesday_spec.rb
+++ wednesday_spec.rb
@@ -1,10 +1,47 @@
 require './wednesday'
 
 QUIET_TWEET = "#WatchWednesday *jashkenas coffee-script docco *cloudhead less *github gollum *creationix postgres-js http://watchwednesday.com/benatkin"
+QUIET_TWEET_DATA =
+  [{:begin=>15, # *jashkenas coffee-script docco
+    :text=>"*jashkenas",
+    :url=>"http://github.com/jashkenas",
+    :end=>27},
+   {:begin=>27,
+    :text=>"coffee-script",
+    :url=>"http://github.com/jashkenas/coffee-script",
+    :end=>41},
+   {:begin=>41,
+    :text=>"docco",
+    :url=>"http://github.com/jashkenas/docco",
+    :end=>47},
+   {:begin=>47, # *cloudhead less
+    :text=>"*cloudhead",
+    :url=>"http://github.com/cloudhead",
+    :end=>58},
+   {:begin=>58,
+    :text=>"less",
+    :url=>"http://github.com/cloudhead/less",
+    :end=>63},
+   {:begin=>63, # *github gollum
+    :text=>"*github",
+    :url=>"http://github.com/github",
+    :end=>71},
+   {:begin=>71,
+    :text=>"gollum",
+    :url=>"http://github.com/github/gollum",
+    :end=>78},
+   {:begin=>78, # *creationix postgres-js
+    :text=>"*creationix",
+    :url=>"http://github.com/creationix",
+    :end=>90}, 
+   {:begin=>90,
+    :text=>"postgres-js",
+    :url=>"http://github.com/creationix/postgres-js",
+    :end=>102}]
 
-describe WednesdayTweet, "#entities" do
-  it "returns the correct data" do
+describe WednesdayTweet, "#render" do
+  it "renders correctly" do
     tweet = WednesdayTweet.new QUIET_TWEET
-    tweet.entities.should == :generated_data
+    tweet.entities.should == QUIET_TWEET_DATA
   end
 end
